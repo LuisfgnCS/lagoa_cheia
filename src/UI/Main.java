248b6ea1d0e829a93ee9483c0983446038b1ad74
Ruan <ruanpablooliveira20@gmail.com>
2024-12-01 19:25:21 -0300

    Quase lá

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.CountDownLatch;
 
-import exceptions.CapacidadeMaximaException;
 import negócios.Bairro;
 import negócios.CaminhaoLixo;
 import negócios.Ponto;
@@ -19,6 +19,8 @@ public class Main {
 		int quantidadeFuncionarios = 3;
 		int quantidadeCaminhoes = 0;
 		int tempoTotalGasto = 0;
+		Scanner sc = new Scanner(System.in);
+		String caminhoArquivo = "/home/ruanp/Documentos/";
 		List<Ponto> vertices = new ArrayList<>();
 		List<Integer> folhas = new ArrayList<>();
 		
@@ -42,37 +44,7 @@ public class Main {
 				w[i][j] = grafo.getW()[i][j];
 			}
 		}
-
-//		while(!grafo.getFolhasMod().isEmpty()) {
-//			int folha = caminhao1.menor(grafo.getFolhasMod()); // procura a menor folha que ainda não foi visitada ou está em rota
-//			PontoDeColeta destino = (PontoDeColeta) grafo.getVertices().get(folha);
-//			List<Integer> percurso = caminhao1.seguirRamo(folha).reversed();
-//			
-//			System.out.println();
-//			System.out.println("Vértices do percurso: " + percurso.toString());
-//			for (int j : percurso) {
-//				System.out.println(grafo.getVertices().get(j).toString());
-//			}
-//			System.out.println(); 
-//			
-//			do {
-//				try {
-//					caminhao1.percorrer(percurso, 1);
-//				} catch (InterruptedException | CapacidadeMaximaException e) {
-//					// TODO Auto-generated catch block
-//				}
-//			}while(destino.getvLixo() != 0);
-//			grafo.getFolhasMod().remove(grafo.getFolhasMod().indexOf(folha));
-//			System.out.println(grafo.getFolhasMod().toString());
-//			System.out.println();
-//			System.out.println("Vértices do percurso: ");
-//			for (int j : percurso) {
-//				System.out.println(grafo.getVertices().get(j).toString());
-//			}
-//			System.out.println();
-//		}
 		
-
 		
 		List<CaminhaoLixo> frota = new ArrayList<>();
 		loopexterno: do {
@@ -92,7 +64,7 @@ public class Main {
 			System.out.println(grafo.getNome());
 			System.out.println((PontoDeColeta) grafo.getVertices().get(1));
 
-			quantidadeFuncionarios = 5;
+			quantidadeFuncionarios = 3;
 			tempoTotalGasto = 0;
 			quantidadeCaminhoes++;
 			frota.clear();
@@ -103,59 +75,54 @@ public class Main {
 			CountDownLatch latch = new CountDownLatch(quantidadeCaminhoes);
 
 			for (int i = 0; i < quantidadeCaminhoes; i++) {
-				frota.add(new CaminhaoLixo(quantidadeFuncionarios, 60.0, grafo, latch));
+				frota.add(new CaminhaoLixo(quantidadeFuncionarios, 40.0, grafo, latch,String.format(caminhoArquivo + "caminhão%d" , i + 1)));
 			}
 
 			for (CaminhaoLixo caminhaolixo : frota) {
 					caminhaolixo.start();
-					Thread.sleep(2000);
 			}
 
 			latch.await();
-
-			for (int j = 0; j < grafo.getW().length; j++) {
-				System.out.println(grafo.getVertices().get(j).toString());
-			}
 			
-			frota.stream().sorted(Comparator.comparing(CaminhaoLixo::getTempoGastoColetandoLixo).reversed());
+
+			frota.stream().sorted(Comparator.comparingInt(CaminhaoLixo::totalTempo).reversed());
 
 			tempoTotalGasto = frota.getFirst().getTempoGastoColetandoLixo() + frota.getFirst().getTempoGastoPercorrendoCaminho();
+			
 
-			if (tempoTotalGasto <= (120)) {
+			System.out.println("=============================================");
+			System.out.println(tempoTotalGasto);
+			
+			if (tempoTotalGasto <= (60*8)) {
 				break loopexterno;
 			}
 
 
 			for (CaminhaoLixo caminhaolixo : frota) {
 				for (int i = 4; i <= 5; i++) {
+					caminhaolixo.getRelatorioIndividual().add("Adicionando mais funcionários!");
 					System.out.println("Testando com " + i + " Funcionários");
+					caminhaolixo.getRelatorioIndividual().add("Testando com " + i + " funcionarios!");
 					tempoTotalGasto -= caminhaolixo.getTempoGastoColetandoLixo();
 					System.out.println("tempo total anterior para coletar lixo " + caminhaolixo.getTempoGastoColetandoLixo());
+					caminhaolixo.getRelatorioIndividual().add("Tempo total anterior para coletar lixo: " + caminhaolixo.getTempoGastoColetandoLixo());
 					tempoTotalGasto += ((caminhaolixo.getTempoGastoColetandoLixo() * (i - 1)) / i);
 					caminhaolixo.setTempoGastoColetandoLixo((caminhaolixo.getTempoGastoColetandoLixo() * (i - 1)) / i);
 					System.out.println();
-					System.out.printf("Novo tempo total para coletar lixo %d", caminhaolixo.getTempoGastoColetandoLixo());
+					System.out.printf("Novo tempo total ára coletar lixo %d", caminhaolixo.getTempoGastoColetandoLixo());
+					caminhaolixo.getRelatorioIndividual().add("Novo tempo total para coletar lixo:" + caminhaolixo.getTempoGastoColetandoLixo());
+					caminhaolixo.getRelatorioIndividual().add("Novo tempo geral: " + caminhaolixo.totalTempo());
 					System.out.println();
-
 					caminhaolixo.setFuncionarios(i);
-					if (tempoTotalGasto <= (120)) {
+					caminhaolixo.construirRelatorio();
+					if (tempoTotalGasto <= (60*8)) {
 						break loopexterno;
 					}
 				}
 			}
-			System.out.println();
-			System.out.println("Folhas do grafo: " + folhas.toString());
-			System.out.println();
-			System.out.println("Vértices do grafo: ");
-			for (int j = 0; j < grafo.getVertices().size(); j++) {
-				System.out.println(grafo.getVertices().get(j).toString());
-			}
-			System.out.println();
 
 		} while (true);
-		for (int j = 0; j < 5; j++) {
-			System.out.println(grafo.getVertices().get(j).toString());
-		}
+
 		System.out.println("A quantidade de caminhões necessárias é de " + frota.size());
 		int contador = 1;
 		for (CaminhaoLixo caminhaoLixo : frota) {
